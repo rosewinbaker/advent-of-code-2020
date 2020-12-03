6b9a3e1dcc115b530ae5813c29313d7dc0d545e1
Rose Baker <rosewin.baker@gmail.com>
2020-12-03 16:09:41 -0600

    day three part one and part two

--- 003/one.py
+++ 003/one.py
@@ -17,57 +17,76 @@ f.close()
 
 # print(lines[0])
 
+tree=0
 trees = []
+total_trees = []
 line_pos = 1
 i=1
 
-def find_tree(terrain, line_pos, trees):
+def find_tree(terrain, line_pos, trees, tree):
     foo = ([pos for pos, char in enumerate(terrain) if char == '#'])
     newfoo = [x+1 for x in foo]
     # print(newfoo)
 
     if line_pos in newfoo:
-        print("We found a tree at line position " + str(line_pos) + " on line " + str(i))
+        # print("We found a tree at line position " + str(line_pos) + " on line " + str(i))
+        tree += 1
         trees.append(i)
 
-while (i<len(lines)):
 
-    terrain = lines[i]
+my_slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
 
-    i += 1
-    line_pos += 3
+j=0
 
-    print("Line # " + str(i) + " + pos # " + str(line_pos))
+while (j<len(my_slopes)): 
+# while (j<2): 
+    right = my_slopes[j][0]
+    down = my_slopes[j][1]
+    print("Checking for slope: " + str(right) + "," + str(down))
+    j += 1
 
-    if line_pos >= (len(terrain) - 2):
+    # print("Lines: " + str(len(lines)))
 
-        remainder = len(terrain) - line_pos
-        print("Encountered end of terrain on line " + str(i) + ". Need to add more. " + str(len(terrain)) + " - " + str(line_pos) + " = " + str(remainder))
+    # print("i #: " + str(i))
 
-        if remainder == 0:
-            find_tree(terrain, line_pos, trees)
-            line_pos = 0
-            print("Setting new line_pos to 0: " + str(line_pos))
-        elif remainder == 1:
-            find_tree(terrain, line_pos, trees)
-            line_pos = -1
-            print("Setting new line_pos to 2: " + str(line_pos))
-        elif remainder == 2:
-            find_tree(terrain, line_pos, trees)
-            line_pos = -2
-            print("Setting new line_pos to 2: " + str(line_pos))
-        elif remainder == -1:
-            line_pos = 3
-            print("Setting new line_pos to 2: " + str(line_pos))
-            find_tree(terrain, line_pos, trees)
-        elif remainder == -2:
-            line_pos = 2
-            print("Setting new line_pos to 1: " + str(line_pos))
-            find_tree(terrain, line_pos, trees)
-    else:
-        find_tree(terrain, line_pos, trees)
-        # print("New line position: " + str(line_pos))
+    while (i<len(lines)):                                                                                                                                                              
 
-print("Total trees encountered: " + str(len(trees)))
+        terrain = lines[i]
+        terrain_length = len(terrain)
+        i += down
+        line_pos += right
+
+        # print("Line # " + str(i) + " + pos # " + str(line_pos))
+
+
+        find_tree(terrain, line_pos, trees, tree)
+
+        if line_pos >= (terrain_length - (right - 1)):
+
+            remainder = terrain_length - line_pos
+            # print("Encountered end of terrain on line " + str(i) + ". Need to add more. " + str(len(terrain)) + " - " + str(line_pos) + " = " + str(remainder))
+
+            line_pos = remainder * -1
+
     
+    print("Total trees encountered: " + str(len(trees)))
+    tree = len(trees)
+    total_trees.append(len(trees))
+    # print(total_trees)
+    trees.clear()
+    i=1
+    line_pos = 1
+    # line
+    
+
+def multiplyList(myList) :
+     
+    # Multiply elements one by one
+    result = 1
+    for x in myList:
+         result = result * x 
+    return result 
+
+print("Final answer: " + str(multiplyList(total_trees)))
+
    
