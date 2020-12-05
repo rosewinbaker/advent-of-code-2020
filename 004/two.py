e0e3b49d4c282f19f966c60a23735f347cf9a1d1
Rose Baker <rosewin.baker@gmail.com>
2020-12-05 14:51:56 -0600

    wait why is this wrong tho

--- 004/two.py
+++ 004/two.py
@@ -20,20 +20,10 @@ while (i<len(passports)):
         pass_dict.clear()
     i += 1
 
-print("# of clean passports: " + str(len(clean_passports)))
+# print("# of clean passports: " + str(len(clean_passports)))
 
-valid = 0
-valid_byr = 0
-valid_iyr = 0
-valid_eyr = 0
-valid_height = 0
-valid_eye_color = 0
-valid_hair_color = 0
-valid_passport_number = 0
 total_valid_points = 0
 but_like_actually_valid = 0
-
-invalid_entries=[]
 
 valid_length = []
 
@@ -53,70 +43,58 @@ for items in valid_length:
         # print("Found a birth year field" )
         if 1920 <= int(clean_passports[j]['byr']) <= 2002:
             # print("byr between 1920 and 2002: " + str(clean_passports[j]['byr']))
-            valid_byr += 1
             total_valid_points += 1
         else:
-            print("Birthyear not between 1920 or 2002: " + str(clean_passports[j]['byr']))
-            valid_byr += 0
+            # print("Birthyear invalid: " + str(clean_passports[j]['byr']))
+            total_valid_points += 0
 
     # Check for issue year 
     if "iyr" in clean_passports[j]:
         # print("Found a issue year field" )
         if 2010 <= int(clean_passports[j]['iyr']) <= 2020:
             # print("iyr between 2010 and 2020: " + str(clean_passports[j]['iyr']))
-            valid_iyr += 1
             total_valid_points += 1
         else:
-            print("Issue year not between 2010 and 2020: " + str(clean_passports[j]['iyr']))
-            valid_iyr += 0
+            # print("Issue year invalid: " + str(clean_passports[j]['iyr']))
+            total_valid_points += 0
             
     # Check for expiration year 
     if "eyr" in clean_passports[j]:
-        # print("Found an expiration year field" )
         if 2020 <= int(clean_passports[j]['eyr']) <= 2030:
             # print("eyr between 2010 and 2020: " + str(clean_passports[j]['eyr']))
-            valid_eyr += 1
             total_valid_points += 1
         else:
-            print("Exp year not between 2020 and 2030: " + str(clean_passports[j]['eyr']))
-            valid_eyr += 0
+            # print("Exp year invalid: " + str(clean_passports[j]['eyr']))
+            total_valid_points += 0
     
     # Check valid height
     if "hgt" in clean_passports[j]:
-        # print("Found a height field" + str(clean_passports[j]['hgt']))
-        # print(str(clean_passports[j]['hgt'][:-2]))
-
         if clean_passports[j]['hgt'][-2:] == "cm":
             if 150 <= int(clean_passports[j]['hgt'][:-2]) <= 193:
                 # print("This is a valid cm height: " + str(clean_passports[j]['hgt']))
-                valid_height += 1
                 total_valid_points += 1
                 # print(clean_passports[j]['hgt'][:-2])
             else:
-                print("Nooope not a valid cm height sorry: " + str(clean_passports[j]['hgt']))
-                valid_height += 0
+                # print("Nooope not a valid cm height sorry: " + str(clean_passports[j]['hgt']))
+                total_valid_points += 0
         elif clean_passports[j]['hgt'][-2:] == "in":
             if 59 <= int(clean_passports[j]['hgt'][:-2]) <= 76:
                 # print("This is a valid in height: " + str(clean_passports[j]['hgt']))
-                valid_height += 1
                 total_valid_points += 1
                 # print(clean_passports[j]['hgt'][:-2])
             else:
-                print("Wrong number of inches: " + str(clean_passports[j]['hgt']))
-                valid_height += 0
+                # print("Wrong number of inches: " + str(clean_passports[j]['hgt']))
+                total_valid_points += 0
 
     # Check for valid eye color 
     valid_eyes = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     if "ecl" in clean_passports[j]:
         if clean_passports[j]['ecl'] in valid_eyes:
             # print("Found a valid eye color: " + clean_passports[j]['ecl'])
-            valid_eye_color += 1
             total_valid_points += 1
         else:
-            print("Invalid eyes: " + clean_passports[j]['ecl'])
-            valid_eye_color += 0
-
-    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            # print("Invalid eyes: " + clean_passports[j]['ecl'])
+            total_valid_points += 0
 
     # Check for vaid hair color 
     if "hcl" in clean_passports[j] and len(clean_passports[j]['hcl']) == 7 and clean_passports[j]['hcl'][0] == "#":
@@ -132,10 +110,10 @@ for items in valid_length:
 
         if all(matched_list) == True:
             # print("Looks like they were all correct")
-            valid_hair_color += 1
             total_valid_points += 1
         else:
-            print("Not a valid hair color: " + clean_passports[j]['hcl'])
+            # print("Not a valid hair color: " + clean_passports[j]['hcl'])
+            total_valid_points += 0
 
     # Check for vaid passport number
     if "pid" in clean_passports[j] and len(clean_passports[j]['pid']) == 9:
@@ -145,8 +123,8 @@ for items in valid_length:
             total_valid_points += 1
             # print("PID can be converted to int.")
         except:
-            valid_passport_number += 0
-            print("That's like not even a number")
+            total_valid_points += 0
+            # print("That's like not even a number")
 
     # print("Totally valid thoooo: " + str(total_valid_points))    
     if total_valid_points >= 7:
