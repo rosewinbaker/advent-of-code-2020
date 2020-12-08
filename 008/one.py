8063ab5387d4941d13f592042e31766a696d8f72
Rose Baker <rosewin.baker@gmail.com>
2020-12-08 17:09:57 -0600

    thanks tony

--- 008/one.py
+++ 008/one.py
@@ -13,6 +13,7 @@ for items in input:
     instruction = input[i][0]
     modifier = input[i][1][0]
     number = input[i][1][1:]
+    both = input[i][1]
     # print("Line #" + str(i) + ": " + instruction + " " + modifier + number)
     lines_visited.append(i)
     # print(lines_visited)
@@ -21,10 +22,12 @@ for items in input:
 
     if instruction == 'acc':
         # print("Accumulate: " + modifier + number)
-        if modifier == "+":
-            accumulator += int(number)
-        elif modifier == "-":
-            accumulator -= int(number)
+        accumulator += int(both)
+
+        # if modifier == "+":
+        #     accumulator += int(number)
+        # elif modifier == "-":
+        #     accumulator -= int(number)
         # print("New accumulator number: " + str(accumulator))
         i += 1
         if i in lines_visited:
@@ -40,17 +43,8 @@ for items in input:
             break
     elif instruction == 'jmp':
         # print("Jumping: " + modifier + number)
-        if modifier == "+":
-            # print("Jumping into the future by " + number + " to line #" + str(i+int(number)) + " from line " + str(i))
-            i += int(number)
-            if i in lines_visited:
-                print("Hey, we've done that before. Stop that.")
-                print("Accumulator number: " + str(accumulator))
-                break
-        elif modifier == "-":
-            # print("Going back in time by  " + number + " to line #" + str(i-int(number)) + " from line " + str(i))
-            i -= int(number)
-            if i in lines_visited:
-                print("Hey, we've done that before. Stop that.")
-                print("Accumulator number: " + str(accumulator))
-                break
+        i += int(both)
+        if i in lines_visited:
+            print("Hey, we've done that before. Stop that.")
+            print("Accumulator number: " + str(accumulator))
+            break
